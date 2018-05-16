efe6e8cfdff657a610e816a60745144aeb3c4c8b
Mark Shull <mshull2@kent.edu>
2018-05-16 16:22:26 -0400

    Add files via upload
    added mobile query and added high performance textures for mobile.

--- slideshowScroller.js
+++ slideshowScroller.js
@@ -9,19 +9,19 @@ var atBottom = false;
 var currentSection = 1;
 var previousSection;
 
-function detectmob() {
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
+function isMobile() {
+  if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+  ){
     return true;
   }
  else {
-    return false;
+   return false;
   }
 }
 
@@ -90,7 +90,11 @@ function translateDown(element) {
 //-----------------------------------
 
 $(document).ready(function(){
-  alert(detectmob());
+  if(isMobile()){
+    $('.section').each(function(){
+      $(this).addClass('mobile');
+    });
+  }
   animationDiv = $('.container');
   //icons
   var up = $('.upArrowContainer');
